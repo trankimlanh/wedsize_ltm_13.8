73076a90b8df345ef4edf622398a981038515727
trankimlanh <trankimlanh2006@gmail.com>
2026-08-13 08:27:55 +0700

    Remove CSS styles and add console log

--- scipt.js
+++ scipt.js
@@ -1,16 +1,1 @@
-body {
-    margin: 0;
-    min-height: 100vh;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    font-family: Arial, sans-serif;
-    background: #f5f5f5;
-}
-
-h1 {
-    font-size: 50px;
-    color: #333;
-}
+console.log("Website đã hoạt động!");
